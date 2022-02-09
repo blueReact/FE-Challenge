af4f80f4e0736c8d09e1a819772b5c2a953a5f5f
Rahul Roy <roycodeman@gmail.com>
2022-02-09 19:34:08 +0530

    New Features: -  Added tabbed functionality to switch from email search to phone search - Added basic validation to check 10 digit phone number - Used phone API - Kept the design responsive as per design specifications

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
+  $('#phone-search-container').hide();
+  $('#btn-show-by-email').click(function(){
+    $('#email-search-container').show();
+    $('#phone-search-container').hide();
+    $(this).addClass("active").siblings().removeClass("active");
+  })
+
+  $('#btn-show-by-phone').click(function(){
+    $('#email-search-container').hide();
+    $('#phone-search-container').show();
+    $(this).addClass("active").siblings().removeClass("active");
+  })
 
   $("#btn-search").on("click", function (e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
-    email = $('input[type="text"]').val().toLowerCase();
+    email = $('#input-search-email').val().toLowerCase();
 
     var x, y;
     regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -14,7 +26,7 @@ $(document).ready(function () {
     }
 
     if (x === true) {
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+      document.querySelector('#input-search-email').parentNode.classList.remove("error");
       const proxyurl = "";
       const url =
         'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
@@ -26,16 +38,16 @@ $(document).ready(function () {
         })
         .catch((e) => console.log(e));
     } else if (x !== true) {
-      document.querySelector('input[type="text"]').parentNode.classList.add("error");
+      document.querySelector('#input-search-email').parentNode.classList.add("error");
     }
   });
 
-  $('input[type="text"]').keypress(function (event) {
-    email = $('input[type="text"]').val().toLowerCase();
+  $('#input-search-email').keypress(function (event) {
+    email = $('#input-search-email').val().toLowerCase();
     regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     if (email.match(regEx)) {
       x = true;
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+      document.querySelector('#input-search-email').parentNode.classList.remove("error");
     } else {
       x = false;
     }
@@ -63,9 +75,52 @@ $(document).ready(function () {
           })
           .catch((e) => console.log(e));
       } else if (x !== true) {
-        document.querySelector('input[type="text"]').parentNode.classList.add("error");
+        document.querySelector('#input-search-email').parentNode.classList.add("error");
       }
     }
   });
+  
+  $("#btn-search-phone").on("click", function (e) {
+    e.preventDefault();
+    localStorage.clear(); //Clears storage for next request
+    let phone = $('#input-search-phone').val().toLowerCase();    
+    let regEx = /^\d{10}$/;    
+
+    getApiData(regEx, phone); //Calling a common function which holds API related logic 
+  });
+
+  $('#input-search-phone').keypress(function (event) {
+    let phone = $('#input-search-phone').val().toLowerCase(); 
+    let regEx = /^\d{10}$/;    
+    let keycode = (event.keyCode ? event.keyCode : event.which);
+    
+    if (keycode == '13') {
+      /**
+       * Makes a request to ltv API to search an specific email address.
+       * If there's a response, it gets stored in the local storage and redirects to results page
+       */
+      event.preventDefault();
+      localStorage.clear(); //Clears storage for next request      
+      getData(regEx, phone); //Calling a common function which holds API related logic 
+    }
+  });
+
+  function getApiData(regEx, phone) {  
+    if (phone.match(regEx)) {
+      document.querySelector('#input-search-phone').parentNode.classList.remove("error");
+      const proxyurl = "";
+      const url =
+        'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phone;
+      fetch(proxyurl + url)
+        .then((response) => response.text())
+        .then(function (contents) {
+          localStorage.setItem("userObject", contents);
+          window.location.href = "result.html";
+        })
+        .catch((e) => console.log(e));
+    } else {
+      document.querySelector('#input-search-phone').parentNode.classList.add("error");
+    }
+  }
 
 });
